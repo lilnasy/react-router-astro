3ed0ff542b34112b93173b675b9c4e3f86df7341
lilnasy <69170106+lilnasy@users.noreply.github.com>
2024-01-15 21:30:15 +0000

    add client-side rendering

--- src/cra/App.tsx
+++ src/cra/App.tsx
@@ -12,19 +12,11 @@ export default function App() {
         "*" route (aka "splat route") to render a "not found" page when someone
         visits an unrecognized URL.
       </p>
-
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="dashboard" element={<Dashboard />} />
-
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
@@ -43,7 +35,7 @@ function Layout() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link reloadDocument to="/about">About</Link>
           </li>
           <li>
             <Link to="/dashboard">Dashboard</Link>
